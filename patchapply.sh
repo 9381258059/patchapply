622043a08359ff546d8c3f12f40a385a7a2a2e16
9381258059 <sreekanthch777@gmail.com>
2023-08-25 04:34:37 -0700

    patchupdate

--- patchapply.sh
+++ patchapply.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+PATCH_DOWNLOAD=/u01/app/oracle/software/12.2.1.2.0-patch
 ORACLE_HOME=/u01/app/oracle/may23/middleware
 PATCHPATH=/u01/app/oracle/may23/middleware/patch12c
 LOG=/u01/app/oracle/may23/middleware/cfgtoollogs/opatch
 rm -f $LOG/opatch`date +%Y-%m-%d_%H`*.log
+cp $PATCH_DOWNLOAD/*Generic.zip $PATCHPATH
 $ORACLE_HOME/OPatch/opatch lsinventory
 DATE=`date +%Y-%m-%d_%H`
 cd  $PATCHPATH
@@ -10,10 +12,13 @@ for j in `ls p*_Generic.zip`
 do
 unzip $j
 done
+rm *.zip
 cd  $PATCHPATH
-for i in $@
+pwd
+for i in *
 do
 x=`cat $LOG/opatch$DATE*.log |grep $i`
+echo $x
 if [ -z "$x" ]
 then
 echo " Patch not applied and applying the patch"
